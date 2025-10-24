39613facd6a15f78564b29b2b38dcb5324a307a0
QuintessentialDigital <88312450+QuintessentialDigital@users.noreply.github.com>
2025-10-24 09:57:51 +0100

    Delete public directory

--- public/assets/app.js
+++
@@ -1,4 +0,0 @@
-(function(){
-  if (!('DR_API_BASE' in window)) window.DR_API_BASE = '';
-  window.apiPath = function(p){ return (window.DR_API_BASE || '') + p; };
-})();
--- public/assets/config.js
+++
@@ -1,4 +0,0 @@
-// Set your API origin here so the front-end calls the right server.
-window.DR_API_BASE = ''; // change if your Render URL differs
-window.apiPath = function(p){ return (window.DR_API_BASE || '') + p; };
-
