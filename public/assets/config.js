c0d581d2f0519a7469a762995663febc8885c044
QuintessentialDigital <88312450+QuintessentialDigital@users.noreply.github.com>
2025-10-24 12:52:26 +0100

    Delete public directory

--- public/assets/config.js
+++
@@ -1,4 +0,0 @@
-// API base helper. If API and site are same origin, return p as-is.
-window.DR_API_BASE = window.DR_API_BASE || '';
-function apiPath(p){ return (window.DR_API_BASE || '') + p; }
-window.apiPath = apiPath;
